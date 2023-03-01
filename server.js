aea5086c6b8176424aeb382f799bb5bd45ce80b1
gonza-ro <gonzalorp12345@gmail.com>
2023-03-01 11:17:09 -0300

    actualizado-01

--- server.js
+++ server.js
@@ -1,92 +1,147 @@
-/* Instanciar nuestras dependencias */
-console.clear();
-import express from "express"
-import handlebars from "express-handlebars"
-import {faker} from '@faker-js/faker'
-import { generar } from "./fake.js"
+import express from "express";
+import ApiProdsSQL from "./api/productos.js";
+import ApiMsjMongoDB from "./api/mensajes.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
-/* Declarar nuestras constantes */
-const app = express()
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
-
-let productos = []
-
-// Establecemos la configuración de handlebars
-app.use(express.static('./handlebars'));
-
 app.engine(
-    "hbs",
-    handlebars.engine  ({
-        extname: "*.hbs",
-        defaultLayout: "index.hbs",
-    })
-    );
-    
-    app.set('view engine', "hbs");
-    app.set("views", "./views");
-    
-    app.get('/', (req, res) => {
-        res.render('datos', )
-        
-    })
-    
-    app.get('/tabla', (req, res) => {
-        // console.log(productos.length)
-        res.render('tabla', {productos});
+  "hbs",
+  handlebars.engine({
+    extname: "*.hbs",
+    defaultLayout: "index.hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static("views/layouts"));
+
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl:
+      'mongodb+srv://pablo:pablo@cluster0.glswgtz.mongodb.net/?retryWrites=true&w=majority',
+    }),
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 60 * 10000 }, // 10 minutos
+    rolling: true
+  })
+);
+
+const apiProdsSQL = new ApiProdsSQL();
+
+const apiMsjMongoDB = new ApiMsjMongoDB();
+
+//PRODUCTOS - MariaDB
+// CORROBORA SI EXISTE LA TABLA "PRODUCTOS", SI NO EXISTE, LA CREA.
+apiProdsSQL.crearTablaProds();
+
+//MENSAJES - MONGODB ATLAS.
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+
+  req.session.username = req.body.name;
+
+  res.redirect("/");
+});
+
+app.get("/", (req, res, next) => {
+
+  if(req.session && req.session.username){
+    req.session.cookie.expires = new Date(Date.now() + 600000);
+
+    const nombre = req.session.username;
+    res.render("inicio", { nombre });
+
+    io.on("connection", (socket) => {
+      console.log("Nuevo cliente conectado");
+  
+      //MSJS
+  
+      apiMsjMongoDB.ListarMsjs().then((msjs) => {
+        socket.emit("mensajes", msjs);
+      });
+  
+      socket.on("nuevo-mensaje", (data) => {
+        apiMsjMongoDB
+          .guardarMsj(data)
+          .then(() => {
+            console.log("Mensaje cargado en la base de datos");
+            return apiMsjMongoDB.ListarMsjs();
+          })
+          .then((msj) => {
+            io.sockets.emit("mensajes", msj);
+            console.log("Vista de mensajes actualizada");
+          });
+      });
+  
+      //PRODS
+  
+      apiProdsSQL.ListarProds().then((prods) => {
+        socket.emit("productos", prods);
+      });
+  
+      socket.on("nuevo-producto", (data) => {
+        apiProdsSQL
+          .guardarProd(data)
+          .then(() => {
+            console.log("Producto cargado en la base de datos");
+            return apiProdsSQL.ListarProds();
+          })
+          .then((prods) => {
+            io.sockets.emit("productos", prods);
+            console.log("Vista de productos actualizada");
+          });
+      });
     });
-    app.post('/datos', (req, res) => {
-        const {nombre, precio, url} = req.body
-        const producto = {
-            'id': id,
-            'nombre':nombre,
-            'precio':precio,
-            'url':url
-        }
-        productos.push(producto_test)
-        console.log(productos)
-        res.redirect('/tabla')
-    });
-    
-    app.get('/api/productos_test', (req, res) =>{
-        console.log(generar)
 
-        res.render('api', {generar} )
-        
-    })
-    
+  }else{
+    res.redirect("/login")
+  };
+});
 
-    app.post('/api', (req, res)=>{
-        const {id, nombre, precio, foto} = req.body
-        const producto = {
-            'id': id,
-            'nombre':nombre,
-            'precio':precio,
-            'foto':foto
-        }
-        generar.push(producto)
-        console.log(producto)
-        res.redirect('/api')
-})
+//MOCK - FAKE PRODS
+app.get("/api/productos-test", (req, res) => {
+  const productosFake = apiProdsSQL.FakeProds();
+  res.render("productos-test", { productosFake });
+});
 
+app.post("/logout", (req, res) => {
+  const nombre = req.session.username;
+  req.session.destroy(error => {
+    if(error){
+      res.send({error: error.message});
+      return;
+    }else{
+      res.render("logout", {nombre})
+    }
+  });
+});
 
+//SERVIDOR
+// ----------------------------------------------|
 
+const PORT = 8080;
 
+const srv = server.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${srv.address().port}`);
+});
 
-
-
-// app.get('/api/productos_test', (req, res) => {
-    //     const cant = Number(req.query.cant) || CANT_PRODUCTOS
-    //     res.json(generarProductos(cant))
-    //     console.log(generarProductos(cant))
-    // })
-    
-    // let objeto = JSON.parse(productos_test)
-    
-    /* Configuración del servidor */
-    
-    const PORT = 8080
-    const server = app.listen(PORT, () => {
-        console.log(PORT,' ||---> Server ON, in port ||---> ', PORT);
-    })
-    server.on('error', error => console.log('Error en el servidor', error));
+server.on("error", (error) => {
+  console.log(`Error en servidor: ${error}`);
+});
